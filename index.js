7f2bb78f39d57ed9730da8661072ab9a93f1ac44
FazleRahmanEvan <evanshakib26@gmail.com>
2022-05-28 21:09:29 +0600

    payment backend

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require ('cors');
 const jwt = require ('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+
 
 
 app.use(cors());
@@ -170,7 +175,22 @@ async function run(){
 
         })
 
+
+
+        app.post('/create-payment-intent',verifyJWT, async(req,res)=>{
+            const product = req.body;
+            const price = product.price;
+            const amount = price*100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount : amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({clientSecret:paymentIntent.client_secret})
+        })
+
      
+
           
         app.get('/orders', verifyJWT, async(req, res)=>{
             const email = req.query.email;
@@ -185,8 +205,15 @@ async function run(){
          }
         })
 
-        app.post('/orders', async (req, res) => {
 
+        app.get('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await orderCollection.findOne(query);
+            res.send(order);
+        })
+
+        app.post('/orders', async (req, res) => {
             const orders = req.body;
             const result = await orderCollection.insertOne(orders);
             res.send(result)
